786e37ca6383714e695b966e6fded045dabed745
emilysupercode <sheil.emily.sc@gmail.com>
2022-10-28 09:31:45 +0200

    updated with more concatination

--- js toLowerCase toUpperCase 1_8/assets/js/main.js
+++ js toLowerCase toUpperCase 1_8/assets/js/main.js
@@ -17,7 +17,7 @@ console.log(outerWords);
 const innerWords = small.replace("is going to", "IS GOING TO");
 console.log(innerWords);
 
-const finalText = betterText.replace("Sam is going to school", "Sam Is Going To School");
+const finalText = betterText.slice(0, 4).concat(betterText.slice(4, 5).toUpperCase()).concat(betterText.slice(5, 7)).concat(betterText.slice(7, 8).toUpperCase()).concat(betterText.slice(8, 13)).concat(betterText.slice(13, 14).toUpperCase()).concat(betterText.slice(14, 16)).concat(betterText.slice(16, 17).toUpperCase()).concat(betterText.slice(17));
 console.log(finalText);
 
 document.write(caps + "<br>" + small + "<br>" + outerWords + "<br>" + innerWords + "<br>" + finalText)
